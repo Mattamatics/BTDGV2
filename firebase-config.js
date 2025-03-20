828c0e1fd9f6f9b50f91f0e1f8a95e2138caf343
Mattamatics <m.mckeever@hotmail.com>
2025-03-20 17:38:28 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Configuration
+// IMPORTANT: Replace the apiKey below with your new restricted API key from the Firebase Console
+const firebaseConfig = {
+  apiKey: "REPLACE_WITH_YOUR_NEW_RESTRICTED_API_KEY",
+  authDomain: "bitcointowerdefensegame.firebaseapp.com",
+  databaseURL: "https://bitcointowerdefensegame-default-rtdb.firebaseio.com",
+  projectId: "bitcointowerdefensegame",
+  storageBucket: "bitcointowerdefensegame.firebasestorage.app",
+  messagingSenderId: "914790506593",
+  appId: "1:914790506593:web:5000cd86898a3b4a79ca20"
+};
